69d328091573d2e0e1de130f27a074dcb540e09c
Alan Vieira Martins <alanhorcrux@gmail.com>
2023-12-03 06:10:28 -0300

    create start project with api using rabbitmq

---
+++ src/api/ApiProcess/Controllers/ProcessController.cs
@@ -0,0 +1,55 @@
+﻿using Domain.Entity;
+using Microsoft.AspNetCore.Mvc;
+using RabbitMQ.Client;
+using System.Text;
+using System.Text.Json;
+
+namespace ApiProcess.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProcessController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok();
+        }
+
+        [HttpPost]
+        public IActionResult Post(string name)
+        {
+            var process = new Process(name);
+
+            var factory = new ConnectionFactory()
+            {
+                HostName = "localhost",
+                Port = 5672,
+                UserName = "guest",
+                Password = "guest"
+            };
+
+            using var connection = factory.CreateConnection();
+            using (var channel = connection.CreateModel())
+            {
+                channel.QueueDeclare(queue: "processQueue",
+                                    durable: false,
+                                    exclusive: false,
+                                    autoDelete: false,
+                                    arguments: null);
+
+                var message = JsonSerializer.Serialize(process);
+
+                var body = Encoding.UTF8.GetBytes(message);
+
+                channel.BasicPublish(
+                    exchange: "",
+                    routingKey: "processQueue",
+                    basicProperties: null,
+                    body: body);
+            };
+
+            return Ok(process);
+        }
+    }
+}
---
+++ src/shared/Domain/Entity/Process.cs
@@ -0,0 +1,26 @@
+﻿namespace Domain.Entity
+{
+    public class Process
+    {
+        public Guid Id { get; set; }
+        public string? Name { get; set; }
+        public DateTime CreateAt { get; set; }
+        public DateTime? ProcessAt { get; set; }
+
+        public Process()
+        {
+        }
+
+        public Process(string name)
+        {
+            Id = Guid.NewGuid();
+            CreateAt = DateTime.Now;
+            Name = name;
+        }
+
+        public override string ToString()
+        {
+            return "{ Id: " + Id + ", Name: " + Name + ", CreateAt: " + CreateAt + ", ProcessAt: " + ProcessAt + " }";
+        }
+    }
+}
