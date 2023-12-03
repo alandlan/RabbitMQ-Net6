932615ca971859965c0e9a2f6f3971d6aa7b6ea6
Alan Vieira Martins <alanhorcrux@gmail.com>
2023-12-03 06:42:47 -0300

    add consumer

--- src/workers/ConsumidorA/Worker.cs
+++ src/workers/ConsumidorA/Worker.cs
@@ -1,3 +1,9 @@
+using Domain.Entity;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+using System.Text.Json;
+
 namespace ConsumidorA
 {
     public class Worker : BackgroundService
@@ -13,11 +19,44 @@ namespace ConsumidorA
         {
             while (!stoppingToken.IsCancellationRequested)
             {
-                if (_logger.IsEnabled(LogLevel.Information))
+                await Task.Delay(5000, stoppingToken);
+
+                var factory = new ConnectionFactory()
                 {
-                    _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
+                    HostName = "localhost",
+                    UserName = "guest",
+                    Password = "guest"
+                };
+
+                using var connection = factory.CreateConnection();
+                using (var channel = connection.CreateModel())
+                {
+                    channel.QueueDeclare(queue: "processQueue",
+                                        durable: false,
+                                        exclusive: false,
+                                        autoDelete: false,
+                                        arguments: null);
+
+                    var consumer = new EventingBasicConsumer(channel);
+
+                    consumer.Received += (model, ea) =>
+                    {
+                        var body = ea.Body.ToArray();
+                        var message = Encoding.UTF8.GetString(body);
+                        var process = JsonSerializer.Deserialize<Process>(message);
+
+                        if(process != null)
+                            process.ProcessAt = DateTime.Now;
+
+                        _logger.LogInformation($"Processo recebido: {process?.ToString()}");
+                    };
+
+                    channel.BasicConsume(queue: "processQueue",
+                                        autoAck: true,
+                                        consumer: consumer);
+
+                    await Task.Delay(5000, stoppingToken);
                 }
-                await Task.Delay(1000, stoppingToken);
             }
         }
     }
