dfe96d7600a5be364e776918747bc2850ffe58fd
Alan Vieira Martins <alanhorcrux@gmail.com>
2023-12-13 13:35:42 -0300

    configurado docker-compose

--- src/api/ApiProcess/Program.cs
+++ src/api/ApiProcess/Program.cs
@@ -2,16 +2,12 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-
-
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen(c => c.SwaggerDoc("v1", new() { Title = "ApiProcess", Version = "v1" }));
 
 var app = builder.Build();
-
-//app.Services.SaveSwaggerJson();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -22,8 +18,6 @@ if (app.Environment.IsDevelopment())
         c.SwaggerEndpoint("/swagger/v1/swagger.json", "ApiProcess v1");
     });
 }
-
-
 
 app.UseHttpsRedirection();
 
